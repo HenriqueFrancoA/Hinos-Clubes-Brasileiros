d4fdbd222ad3c83a2aeb173d779c990510a6b2e7
Henrique Franco <henriquefrancoaraujo@gmail.com>
2024-04-05 09:07:04 -0300

    ignore

--- lib/models/link_import.dart
+++ lib/models/link_import.dart
@@ -1,3 +1,4 @@
 class Link {
-  static String import = '';
+  static String import =
+      'https://www.dropbox.com/scl/fi/983vocwkabj6uoeel2brd/assets.zip?rlkey=8by4k7k5qpl7x5dolar4acjkl&dl=0&dl=1';
 }
